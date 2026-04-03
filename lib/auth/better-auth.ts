160a4b5023c3045c02e3c28f6b6e0614e503cc8c
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-04-03 09:48:58 +0545

    add better auth config

---
+++ lib/auth/better-auth.ts
@@ -0,0 +1,39 @@
+import { betterAuth } from 'better-auth';
+import { prisma } from '@/lib/db/prisma';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import bcrypt from "bcryptjs"
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "mongodb"
+    }),
+    emailAndPassword: {
+        enabled: true,
+        async hash(password: string) {
+            // Implement password hashing logic here
+            return await bcrypt.hash(password, 10);
+        },
+        async verify(password: string, hash: string) {
+            // Implement password verification logic here
+            return await bcrypt.compare(password, hash);
+        }
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        },
+    },
+    session: {
+        cookieCache: {
+            maxAge: 7 * 24 * 60 * 60, // 7 days
+        },
+    },
+    user: {
+        modelName: 'user',
+    },
+});
