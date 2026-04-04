6fa11e031fc2a97a464f4be684dda575c616b2f7
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-04-04 10:27:41 +0545

    add footer component

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaFacebook, FaInstagram } from 'react-icons/fa'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="mx-auto max-w-7xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <div>
+                        <h3 className='text-primary font-bold text-2xl'>HerboSpot</h3>
+                        <p className='text-sm text-gray-500'>Your trusted source for herbal remedies and natural wellness solutions.</p>
+                    </div>
+
+                    <div className='flex gap-5'>
+                        <FaFacebook size={24} />
+                        <FaInstagram size={24} />
+                    </div>
+                </div>
+
+                <p className="text-xs text-gray-500">© 2022. Company Name. All rights reserved.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer  
