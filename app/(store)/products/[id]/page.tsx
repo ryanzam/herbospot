1dc6cbf07ffb3cc5b205226286fb773171df6a35
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-04-23 18:03:32 +0545

    update products page

--- app/(store)/products/[id]/page.tsx
+++ app/(store)/products/[id]/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import { useParams, useRouter } from 'next/navigation';
-import Image from 'next/image';
 import SkeletonLoader from '@/components/ui/SkeletonLoader';
 import toast from 'react-hot-toast';
 import { FiMinus, FiPlus, FiShoppingCart, FiHeart } from 'react-icons/fi';
