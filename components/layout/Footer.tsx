85626b39363f177e4573a09ea140007f2065b676
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-04-24 09:39:28 +0545

    update footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,6 +9,11 @@ const Footer = () => {
                     <div>
                         <h3 className='text-primary font-bold text-2xl'>HerboSpot</h3>
                         <p className='text-sm text-gray-500'>Your trusted source for herbal remedies and natural wellness solutions.</p>
+                        <address className='text-sm text-gray-500 not-italic'>
+                            Gorkha 34100, Nepal<br />
+                            Phone: +977-9834560001<br />
+                            Email: info@herbospot.com
+                        </address>
                     </div>
 
                     <div className='flex gap-5'>
@@ -17,7 +22,7 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <p className="text-xs text-gray-500">© 2022. Company Name. All rights reserved.</p>
+                <p className="text-xs text-gray-500">© 2022, HerboSpot. All rights reserved.</p>
             </div>
         </footer>
     )
