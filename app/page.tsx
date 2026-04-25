2c51c6a4318909225511d260321290021224c186
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-04-25 10:50:39 +0545

    make homepage responsive a bit

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,18 @@ export default function Home() {
     <div className="container-custom py-4">
       <div className="grid grid-cols-[30%_70%] gap-8 items-center">
         <div className="grid grid-rows-[40%_60%] gap-8">
-          <div>
+          <div className="relative md:w-full">
             <Image
               src="https://images.pexels.com/photos/2575372/pexels-photo-2575372.jpeg"
               alt="Hero Image"
-              width={500}
-              height={500}
+              fill
               className="w-full h-auto rounded-4xl"
               loading="eager"
             />
           </div>
-          <div className="rounded-4xl bg-secondary p-8">
-            <h1 className="uppercase font-extrabold text-white text-3xl">Find your perfect herbs at HerbSpot</h1>
-            <p className="text-lg text-white/50 mt-4">
+          <div className="relative rounded-4xl bg-secondary p-8">
+            <h1 className="uppercase font-extrabold text-white text-lg md:text-3xl">Find your perfect herbs at HerbSpot</h1>
+            <p className="text-sm md:text-lg text-white/50 mt-4">
               Discover a wide variety of high-quality herbs and natural remedies to support your health and wellness journey.
             </p>
             <Link href="/products" >
