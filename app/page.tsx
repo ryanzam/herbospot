96c8ab0a0f99a8b4c6ad6f0b2db4ba5fc96c6a95
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-04-25 12:06:42 +0545

    make page responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import { BiAward, BiBox } from "react-icons/bi";
+import { SiFresh } from "react-icons/si";
 
 export default function Home() {
   return (
@@ -21,7 +23,7 @@ export default function Home() {
               Discover a wide variety of high-quality herbs and natural remedies to support your health and wellness journey.
             </p>
             <Link href="/products" >
-              <button className="btn-primary mt-8 mb-16">Explore Products</button>
+              <button className="btn-primary mt-8 mb-16">Explore Herbs</button>
             </Link>
           </div>
         </div>
@@ -38,17 +40,16 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="my-32">
-        <h2 className="text-6xl text-center text-primary">We are an independent herb choice for high-quality, organic herbs</h2>
+      <div className="mt-20 mb-16 md:my-32">
+        <h2 className="text-3xl md:text-6xl text-center text-primary">We are an independent herb choice for high-quality, organic herbs</h2>
 
         <div className="grid grid-cols-3 gap-8 mt-8">
-          <div className="relative overflow-hidden">
+          <div className="relative overflow-hidden ">
             <Image
               src="https://images.pexels.com/photos/34786539/pexels-photo-34786539.jpeg"
               alt="Hero Image"
-              width={200}
-              height={200}
-              className="w-full rounded-4xl object-cover h-50"
+              fill
+              className="w-full rounded-4xl object-cover h-30 md:h-50"
               loading="eager"
             />
             <div className="absolute inset-0 bg-black/40 h-50 rounded-4xl"></div>
@@ -84,6 +85,29 @@ export default function Home() {
           <Link href="/products" className="col-span-3 flex justify-center">
             <button className="btn-primary mt-8 mb-16">View all Herbs</button>
           </Link>
+        </div>
+      </div>
+
+      <div className="bg-primary/20 p-10">
+        <div className="flex flex-col">
+          <div className="text-2xl text-primary font-bold text-center my-8">
+            <p>HerboSpot Quality Promise</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 w-full md:flex-row  items-center justify-between">
+          <div className="text-2xl text-primary font-bold flex items-center bg-white rounded-2xl py-5 px-3">
+            <BiAward size={40} className="font-extrabold" />
+            <p className="text-sm md:text-lg">High Quality Herbs</p>
+          </div>
+          <div className="text-2xl text-primary font-bold flex items-center bg-white rounded-2xl py-5 px-3">
+            <SiFresh size={40} className="font-extrabold" />
+            <p className="text-sm md:text-lg">Freshness preserved</p>
+          </div>
+          <div className="text-2xl text-primary font-bold flex items-center bg-white rounded-2xl py-5 px-3">
+            <BiBox size={40} className="font-extrabold" />
+            <p className="text-sm md:text-lg">Easy refunds and returns</p>
+          </div>
         </div>
       </div>
     </div>
